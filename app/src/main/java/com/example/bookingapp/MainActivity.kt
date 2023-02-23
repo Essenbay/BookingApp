e8b24b7957ba281797b99951c88735fef7b366e6
Essenbay Assel <esenbay.asel@mail.ru>
2023-02-23 14:30:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/bookingapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.bookingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        bottomNavigationView.setupWithNavController(navController)
+
+
+    }
+}
